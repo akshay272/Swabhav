735baa88053ad4d30d698abf00e4a80c0731c9f4
akshay272 <64827755+akshay272@users.noreply.github.com>
2021-11-13 19:33:22 +0530

    updated SOLID folder

---
+++ SOLID/DIPSolution/DIPVoilationApp/HighLevelModule/TaxCalculatorcs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIPVoilationApp.HighLevelModule;
+using DIPVoilationApp.LowLevelModule;
+namespace DIPVoilationApp.HighLevelModule
+{
+    class TaxCalculatorcs
+    {
+        private LogType _policy;
+        public TaxCalculatorcs(LogType logPolicy)
+        {
+            this._policy = logPolicy;
+
+            if (_policy == LogType.DB)
+            {
+                Console.WriteLine(new DBLogger().WriteLine());
+            }
+            else
+            {
+                Console.WriteLine(new FileLogger().WriteLine());
+            }
+        }
+
+        public int Calculate(int value1, int value2)
+        {
+            int result=0;
+            try
+            {
+                result = value1 / value2;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            return result;
+        }
+    }
+}
